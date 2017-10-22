7f33a7747b16042c0da3fd770de33363755eef45
farruhhabibullaev <germany-2013@mail.ru>
2017-10-21 20:21:15 -0500

    Solutions Skipping and Limit with Stream

--- src/lab9/homework/assignment/partOne/three/EmployeeInfoBetter.java
+++ src/lab9/homework/assignment/partOne/three/EmployeeInfoBetter.java
@@ -1,26 +1,35 @@
 package lab9.homework.assignment.partOne.three;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.function.*;
 
 public class EmployeeInfoBetter {
 	enum SortMethod {BYNAME, BYSALARY};
-	Function<Employee, String> byName = e -> e.getName();
-	Function<Employee, Integer> bySalary = e -> -e.getSalary();
-	
-	public void sort(List<Employee> emps, final SortMethod method) {
-		if(method == SortMethod.BYNAME) {
-			Collections.sort(emps, Comparator.comparing(byName).thenComparing(bySalary));
-		} else {
-			Collections.sort(emps, Comparator.comparing(bySalary).thenComparing(byName));
-		}			
+	static Map<SortMethod, Pair> sortDescriminate = new HashMap<>();
+
+	static {
+		sortDescriminate.put(SortMethod.BYNAME, new Pair<>(Employee::getName, Employee::getSalary));
+		sortDescriminate.put(SortMethod.BYSALARY, new Pair<>( Employee::getSalary, Employee::getName));
 	}
-	
-	
-	
+
+	public void sort(List<Employee> emps, final SortMethod method) {
+		Collections.sort(emps,Comparator.comparing(sortDescriminate.get(method).first)
+				.thenComparing(sortDescriminate.get(method).second));
+
+	}
+
+	static class Pair<T extends Comparable<? super T>, U extends Comparable<? super U>>{
+
+		final Function<Employee,T> first;
+		final Function<Employee,U> second;
+
+		public Pair(Function<Employee, T> first, Function<Employee,U> second) {
+			this.first = first;
+			this.second = second;
+		}
+
+	}
+
 	public static void main(String[] args) {
 		List<Employee> emps = new ArrayList<>();
 		emps.add(new Employee("Joe", 100000));
