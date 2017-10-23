9c31345b6402779b9aa33be43efa6b9492863719
farruhhabibullaev <germany-2013@mail.ru>
2017-10-23 08:31:33 -0500

    all the assignment 10

---
+++ src/lab10/homework/assignment/init.java
@@ -0,0 +1,4 @@
+package lab10.homework.assignment;
+
+public class init {
+}
---
+++ src/tdd/UnitTestExample/myUnitTestcase.java
@@ -0,0 +1,18 @@
+package tdd.UnitTestExample;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+
+public class myUnitTestcase {
+
+	@Test
+	public void test() {
+		assertEquals("Hello testing","Hello testing");
+	}
+	
+}
