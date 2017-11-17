8ac7d6e045f671bce72d3f04ed39d935722dfa25
farruhhabibullaev <germany-2013@mail.ru>
2017-11-17 15:18:11 -0600

    New Updates

--- src/NewMPP_ASSN/excirces/prob5/exam/Merge.java
+++ src/NewMPP_ASSN/excirces/prob5/exam/Merge.java
@@ -16,6 +16,8 @@ public class Merge {
 
 
 
+
+
 	}
 
 
---
+++ src/NewMPP_ASSN/excirces/prob5/exam/Merger.java
@@ -0,0 +1,11 @@
+package NewMPP_ASSN.excirces.prob5.exam;
+
+import java.util.List;
+
+public class Merger<T extends Comparable<T>, U extends Comparable<U>> {
+
+    List<?> merge(List<T> a, List<U> b){
+        return null;
+    }
+
+}
