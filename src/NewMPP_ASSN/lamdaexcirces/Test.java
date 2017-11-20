c1b37634046e2bfcffa5433d65799d1ac6cfe154
farruhhabibullaev <germany-2013@mail.ru>
2017-11-19 20:56:01 -0600

    New Updates

--- src/NewMPP_ASSN/lamdaexcirces/Test.java
+++ src/NewMPP_ASSN/lamdaexcirces/Test.java
@@ -1,20 +1,29 @@
 package NewMPP_ASSN.lamdaexcirces;
 
+import java.util.Comparator;
 import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class Test {
-	
-	public static void main(String [] string){
-		Function<Employee, String> employee = e->e.getName();
-		Function<Employee, String> employee2 = Employee::getName;
-		
-		BiConsumer<Employee, String> empSet = (e,s)->e.setName(s);
-		
-//		Function<String, String> comparing = new Function<String, String>{
-//			
-//		}
-		
-	}
+
+    public static void main(String[] string) {
+        Function<Employee, String> employee = e -> e.getName();
+        Function<Employee, String> employee2 = Employee::getName;
+
+        BiConsumer<Employee, String> empSet = (e, s) -> e.setName(s);
+
+        BiFunction<String,String,Integer> comparing = new BiFunction<String, String, Integer>() {
+            @Override
+            public Integer apply(String s, String s2) {
+                return s.compareTo(s2);
+            }
+        };
+
+        BiFunction<String, String, Integer> comparing2 = (String s, String s2)-> s.compareTo(s2);
+
+        BiFunction<Integer, Integer, Double> mathPower = (i1,i2)->Math.pow(i1,i2);
+
+        
+    }
 }
